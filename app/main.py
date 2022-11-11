3ddde1b6a6b03efd2b0a4424f6f60fea93b7e8eb
Spanarchian <spanarchian@gmail.com>
2022-11-11 15:30:30 +0000

    ADDED: CORS resolution

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from .endpoints.event import router as eventrouter
 from .endpoints.blog import router as blogrouter
 from .endpoints.video import router as videorouter
@@ -21,6 +23,21 @@ class Philosophers(BaseModel):
     nationality: str
  
 app = FastAPI()
+origins = [
+    "http://baselinefastapi.herokuapp.com/",
+    "https://baselinefastapi.herokuapp.com/",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8100",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(eventrouter)
 app.include_router(blogrouter)
 app.include_router(videorouter)
